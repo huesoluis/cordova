bdb099a52745fd09676ac51acdfe3ac7395c05f2
huesoluis <huesoluis@gmail.com>
2016-09-16 12:19:14 +0200

    mod1 cordova

--- www/js/index.js
+++ www/js/index.js
@@ -5,11 +5,18 @@ var app = {
                  console.log("inicial...");
                document.addEventListener('deviceready', this.onDeviceReady, false);
          console.log("inicializada");
+    $("#btnfoto").click(function(event){
+
+                console.log("echando foto");
+                alert("Echando foto");
+
+
+            })
+ 
     },
     onDeviceReady: function() {
             console.log("el disp esta listo");
-
-    }
+               }
 };
 
 app.initialize();
